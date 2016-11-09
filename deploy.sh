e8efac56bfc780add8dd138da97be7e4fe2b7d48
Phil Mander <philip.mander@gmail.com>
2016-11-09 18:01:07 +0100

    Deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,24 @@
 host="philip@139.162.158.136"
 site="periodic-table"
 
-rm $site-1.0.0.tgz
+rm $site-1.1.1.tgz
 npm pack
 
 #copy and unpack
-scp /home/philip/Me/$site/$site-1.0.0.tgz $host:/home/philip/deploy
+scp /home/philip/Me/$site/$site-1.1.1.tgz $host:/home/philip/deploy
 ssh $host rm /home/philip/deploy/$site/package -r
-ssh $host tar -zxf /home/philip/deploy/$site-1.0.0.tgz  -C /home/philip/deploy/$site
+ssh $host tar -zxf /home/philip/deploy/$site-1.1.1.tgz  -C /home/philip/deploy/$site
 
 #move to new
 ssh $host -t "rm /home/philip/www/\"$site\"_new -r"
 ssh $host cp /home/philip/deploy/$site/package/ /home/philip/www/"$site"_new -r
-ssh $host -t "cd /home/philip/www/\"$site\"_new"
+ssh $host -t "cd /home/philip/www/\"$site\"_new && npm rebuild"
 
 #stop and create rollback
-ssh $host pm2 stop /home/philip/www/$site/app.js
+ssh $host pm2 stop /home/philip/www/$site/server.js
 ssh $host -t "rm /home/philip/www/\"$site\"_old -rf"
 ssh $host mv /home/philip/www/"$site" /home/philip/www/"$site"_old
 
 #deploy new and start
 ssh $host mv /home/philip/www/"$site"_new /home/philip/www/"$site"
-ssh $host pm2 start /home/philip/www/"$site"/app.js --name="$site"
+ssh $host pm2 start /home/philip/www/"$site"/server.js --name="$site"
